94003ca77cd8f6b4d7deaa424420377f6e89a8a9
Saltan <saltan@nelta.de>
2023-04-17 14:14:14 +0200

    My First Commit

--- src/main/java/pages/SA_TMCompose.java
+++ src/main/java/pages/SA_TMCompose.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class SA_TMCompose {
@@ -9,28 +10,94 @@ public class SA_TMCompose {
 
     public SA_TMCompose(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+        //PageFactory.initElements(driver, this);
     }
-    private final By LoginButton = By.id("yt1");
+    private final By LoginButton = By.name("yt1");
     private final By Usernamefield = By.id("LoginForm_username");
-    public void LoginButtonClick(){
-        this.driver.findElement(LoginButton).click();
+    private final By Passwordfield = By.id("LoginForm_password");
+    private final By AcceptCookies = By.id("sp_message_iframe_757749");
+    private final By Anmeldebutton = By.xpath("//*[@id=\"login\"]/span");
+    private final By framecookies = By.id("sp_message_iframe_757749");
+    private final By ExceptButton = By.xpath("//*[@id=\"notice\"]/div[3]/div[2]/button");
+    private final By ClickMeinTM = By.xpath("//*[@id=\"main\"]/header/nav/ul/li[7]/span");
+    private final By ClickTraumelf = By.xpath("//*[@id=\"main\"]/header/nav/ul/li[7]/div/div[1]/ul/li[2]/ul/li[3]/a");
+    private final By ClickElfErstellen = By.xpath("//*[@id=\"main\"]/main/div[4]/div[2]/div[1]/div/a");
+    private final By ClickFormation = By.name("traumelf_taktik");
+    private final By ChooseFormation = By.xpath("//*[@id=\"Begegnung_taktik_id_g\"]/option[15]");
+    private final By ChooseTW = By.xpath("//*[@id=\"field\"]/li[11]/div/img");
+    private final By ChooseRV = By.xpath("//*[@id=\"field\"]/li[1]/div/img");
+    private final By ChooseRIV = By.xpath("//*[@id=\"field\"]/li[3]/div/img");
+    private final By ChooseLIV = By.xpath("//*[@id=\"field\"]/li[2]/div/img");
+    private final By ChooseLV = By.xpath("//*[@id=\"field\"]/li[4]/div/img");
+    private final By ChooseRZM = By.xpath("//*[@id=\"field\"]/li[5]/div/img");
+    private final By ChooseLZM = By.xpath("//*[@id=\"field\"]/li[6]/div/img");
+    private final By ChooseZOM = By.xpath("//*[@id=\"field\"]/li[7]/div/img");
+    private final By ChooseRF = By.xpath("//*[@id=\"field\"]/li[8]/div/img");
+    private final By ChooseST = By.xpath("//*[@id=\"field\"]/li[9]/div/img");
+    private final By ChooseLF = By.xpath("//*[@id=\"field\"]/li[10]/div/img");
+    private final By SearchPlayer = By.id("searchPlayer");
+    private final By SelectPLayer = By.xpath("//*[@id=\"traumelf-form\"]/tm-lineup/div[1]/div[2]/ul/li");
+    private final By SelectPlayerEins = By.xpath("//*[@id=\"traumelf-form\"]/tm-lineup/div[1]/div[2]/ul/li[1]");
+    private final By SelectKaderPlayerOne = By.xpath("//*[@id=\"squad\"]/li");
+    private final By SelectKaderPlayerTwo = By.xpath("//*[@id=\"squad\"]/li[2]");
+    private final By SelectKaderPlayerDrei = By.xpath("//*[@id=\"squad\"]/li[1]");
+    private final By SelectKaderPlayerVier = By.xpath("//*[@id=\"squad\"]/li[4]");
 
 
+
+
+
+    public WebElement getframecookies(){
+        return driver.findElement(framecookies);
     }
-    public void LoginProcess(){
-        this.driver.findElement(LoginButton).click();
-        this.driver.findElement(Usernamefield).sendKeys("Saltan667");
-
+    public WebElement getexceptbutton(){
+        return driver.findElement(ExceptButton);
     }
 
-    public void OpenURL(){
-        this.driver.get("https://www.transfermarkt.de/");
-
+    public WebElement getAnmeldebutton(){
+        return driver.findElement(Anmeldebutton);
     }
-    public void OpenURLLogin(){
-        this.driver.get("");
 
-
+    public WebElement getUsernamefield(){
+        return driver.findElement(Usernamefield);
     }
+    public WebElement getPasswordfield(){
+        return driver.findElement(Passwordfield);
+    }
+    public WebElement getLoginButton(){
+        return driver.findElement(LoginButton);
+    }
+    public WebElement getClickMeinTM(){
+        return driver.findElement(ClickMeinTM);
+    }
+    public WebElement getClickTraumelf(){
+        return driver.findElement(ClickTraumelf);
+    }
+    public WebElement getClickElfErstellen(){
+        return driver.findElement(ClickElfErstellen);
+    }
+    public final WebElement getClickFormation(){
+        return driver.findElement(ClickFormation);
+    }
+    public final WebElement getChooseFormation(){
+        return driver.findElement(ChooseFormation);
+    }
+    public final WebElement getChooseTW() {return  driver.findElement(ChooseTW);}
+    public final WebElement getChooseRV() {return  driver.findElement(ChooseRV);}
+    public final WebElement getChooseRIV() {return  driver.findElement(ChooseRIV);}
+    public final WebElement getChooseLIV() {return  driver.findElement(ChooseLIV);}
+    public final WebElement getChooseLV() {return  driver.findElement(ChooseLV);}
+    public final WebElement getChooseRZM() {return  driver.findElement(ChooseRZM);}
+    public final WebElement getChooseLZM() {return  driver.findElement(ChooseLZM);}
+    public final WebElement getChooseZOM() {return  driver.findElement(ChooseZOM);}
+    public final WebElement getChooseRF() {return  driver.findElement(ChooseRF);}
+    public final WebElement getChooseST() {return  driver.findElement(ChooseST);}
+    public final WebElement getChooseLF() {return  driver.findElement(ChooseLF);}
+    public final WebElement getSearchPlayer() {return driver.findElement(SearchPlayer);}
+    public final WebElement getSelectPLayer() {return driver.findElement(SelectPLayer);}
+    public final WebElement getSelectKaderPlayerOne() {return driver.findElement(SelectKaderPlayerOne);}
+    public final WebElement getSelectKaderPlayerTwo() {return driver.findElement(SelectKaderPlayerTwo);}
+    public final WebElement getSelectPLayerEins() {return driver.findElement(SelectPlayerEins);}
+    public final WebElement getSelectKaderPlayerDrei() {return driver.findElement(SelectKaderPlayerDrei);}
+    public final WebElement getSelectKaderPlayerVier() {return driver.findElement(SelectKaderPlayerVier);}
 }
--- src/test/java/steps/SA_MS056Steps.java
+++ src/test/java/steps/SA_MS056Steps.java
@@ -3,49 +3,149 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import pages.SA_TMCompose;
 import qa.factory.DriverFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public class SA_MS056Steps {
 WebDriver driver = DriverFactory.getDriver();
 SA_TMCompose Transfer = new SA_TMCompose(driver);
 
     @Given("User opens the Website")
-    public void user_opens_the_website() {
-        Transfer.OpenURL();
+    public void user_opens_the_website() throws InterruptedException {
+        driver.get("https://www.transfermarkt.de/");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
-        Transfer.LoginButtonClick();
-        Transfer.LoginProcess();
-
-
+        driver.switchTo().frame(Transfer.getframecookies());
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getexceptbutton().click();
+        Transfer.getAnmeldebutton().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getUsernamefield().sendKeys("Saltan67");
+        Transfer.getPasswordfield().sendKeys("Nelta66GmbH!");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getLoginButton().click();
 
 
     }
     @Given("User is logged in")
     public void user_is_logged_in() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("User clicks on “Mein TM”")
-    public void user_clicks_on_mein_tm() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_clicks_on_mein_tm() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getClickMeinTM().click();
     }
     @When("Clicks on “Meine Traumelf”")
-    public void clicks_on_meine_traumelf() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void clicks_on_meine_traumelf() throws InterruptedException {
+        Transfer.getClickMeinTM().click();
+        Transfer.getClickTraumelf().click();
     }
     @When("Clicks on “Neue Elf erstellen”")
     public void clicks_on_neue_elf_erstellen() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getClickElfErstellen().click();
     }
     @Then("User can compose his dream starting eleven")
     public void user_can_compose_his_dream_starting_eleven() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Transfer.getClickFormation().click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getChooseFormation().click();
+    //TW
+        Transfer.getChooseTW().click();
+        Transfer.getSearchPlayer().sendKeys("Iker Casillas");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayer().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerOne().click();
+
+    //RV
+        Transfer.getChooseRV().click();
+        Transfer.getSearchPlayer().sendKeys("Gökhan Gönül");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayer().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerOne().click();
+
+    //RIV
+        Transfer.getChooseRIV().click();
+        Transfer.getSearchPlayer().sendKeys("Sergio Ramos");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerDrei().click();
+
+    //LIV
+        Transfer.getChooseLIV().click();
+        Transfer.getSearchPlayer().sendKeys("Min-Jae Kim");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerTwo().click();
+
+    //LV
+        Transfer.getChooseLV().click();
+        Transfer.getSearchPlayer().sendKeys("Marcelo Vieira da Silva Júnior");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayer().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerDrei().click();
+
+    //RZM
+        Transfer.getChooseRZM().click();
+        Transfer.getSearchPlayer().sendKeys("Modric");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerTwo().click();
+
+    //LZM
+        Transfer.getChooseLZM().click();
+        Transfer.getSearchPlayer().sendKeys("Casemiro");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerDrei().click();
+
+    //ZOM
+        Transfer.getChooseZOM().click();
+        Transfer.getSearchPlayer().sendKeys("De Bruyne");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerDrei().click();
+
+
+    //RF
+        Transfer.getChooseRF().click();
+        Transfer.getSearchPlayer().sendKeys("Shaqiri");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerTwo().click();
+
+    //ST
+        Transfer.getChooseST().click();
+        Transfer.getSearchPlayer().sendKeys("Benzema");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerDrei().click();
+
+    //LF
+        Transfer.getChooseLF().click();
+        Transfer.getSearchPlayer().sendKeys("Ronaldo");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        Transfer.getSelectPLayerEins().click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        Transfer.getSelectKaderPlayerVier().click();
+
+
+
     }
 
 
